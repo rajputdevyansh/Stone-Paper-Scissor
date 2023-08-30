b2fa8fffc5113d34e3a41b325712f39d839ec4b6
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2023-08-31 04:07:29 +0530

    commit

--- env/src.py
+++ env/src.py
@@ -1,16 +1,70 @@
 import cv2
 import cvzone
+import random
+from cvzone.HandTrackingModule import HandDetector
+import time
 
 cap = cv2.VideoCapture(0)
 cap.set(3,640)
 cap.set(4,480)
+
+detector = HandDetector(maxHands=1,detectionCon=0.8)
+
+timer=0
+stateResult=False
+startGame=False
+scores=[0,0] #AI,Player
 
 while True:
     imagebg=cv2.imread("Resources/BG.png")
     success, img = cap.read()
     imgScaled = cv2.resize(img, (0, 0),None,0.875,0.875)
     imgScaled=imgScaled[:,80:480]
-    cv2.imshow("Image", img)
+
+    #find the hand
+    hands, img = detector.findHands(imgScaled)
+
+    if startGame:
+        if stateResult is False:
+            timer=time.time()-initialTime
+            cv2.putText(imagebg, str(int(timer)), (605,435), cv2.FONT_HERSHEY_PLAIN, 6, (255, 0, 255), 4)
+
+            if timer>3:
+                stateResult=True
+                timer=0
+
+                if hands:
+                    playerMove=None
+                    hand=hands[0]
+                    fingers=detector.fingersUp(hand)
+                    if fingers == [0, 0, 0, 0, 0]:
+                        playerMove=1
+                    if fingers == [1, 1, 1, 1, 1]:
+                        playerMove=2
+                    if fingers == [0, 1, 1, 0, 0]:
+                        playerMove=3
+
+                    randomNumber=random.randint(1,3)
+                    imgAI=cv2.imread(f"Resources/{randomNumber}.png",cv2.IMREAD_UNCHANGED)
+                    imagebg = cvzone.overlayPNG(imagebg, imgAI, [149, 310])
+
+                    if (playerMove==1 and randomNumber==3) or (playerMove==2 and randomNumber==1) or (playerMove==3 and randomNumber==2):
+                        scores[1]+=1
+
+                    if(playerMove==3 and randomNumber==1) or (playerMove==1 and randomNumber==2) or (playerMove==2 and randomNumber==3):
+                        scores[0]+=1
+
+    imagebg[234:654,795:1195]=imgScaled
+    if stateResult:
+        imgBG=cvzone.overlayPNG(imagebg, imgAI, [149, 310])
+
+    cv2.putText(imagebg, str(scores[0]), (410, 215), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 6)
+
     cv2.imshow("Imagebg", imagebg)
-    cv2.imshow("ImageScaled", imgScaled)
-    cv2.waitKey(1)
+    key=cv2.waitKey(1)
+    if key==ord('q'):
+        break
+    if key==ord('s'):
+        startGame=True
+        initialTime=time.time()
+        stateResult=False
